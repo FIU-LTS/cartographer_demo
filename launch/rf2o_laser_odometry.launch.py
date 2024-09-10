b9f619fea45d7d4eaa25aa8c88363e0f82f2659e
Brendon Cintas <bcintas@fiu.edu>
2024-09-10 17:42:21 +0000

    Uses Rtabmap_odom for ICP

--- launch/rf2o_laser_odometry.launch.py
+++ launch/rf2o_laser_odometry.launch.py
@@ -16,18 +16,37 @@ def generate_launch_description():
 
     return LaunchDescription([
 
-            Node(
-                package='rf2o_laser_odometry',
-                executable='rf2o_laser_odometry_node',
-                name='rf2o_laser_odometry',
-                output='screen',
-                parameters=[{
-                    'laser_scan_topic' : '/scan',
-                    'odom_topic' : '/odom',
-                    'publish_tf' : True,
-                    'base_frame_id' : 'base_link',
-                    'odom_frame_id' : 'odom',
-                    'init_pose_from_topic' : '',
-                    'freq' : 10.0}],
-            ),
+             Node(
+            package='rtabmap_odom', executable='icp_odometry', output='screen',
+            parameters=[{
+              'frame_id':'base_link',
+              'odom_frame_id':'odom',
+              'publish_tf': False, 
+              'wait_for_transform':0.4,
+              'expected_update_rate':25.0,
+              'use_sim_time': False,
+            }],
+            remappings=[
+              ('scan_cloud', '/points'),
+              ('imu', '/imu'),
+              ('odom', '/icp/odometry'),
+            ],
+            arguments=[
+              'Icp/PointToPlane', 'true',
+              'Icp/Iterations', '10',
+              'Icp/VoxelSize', '0.1',
+              'Icp/Epsilon', '0.001',
+              'Icp/PointToPlaneK', '20',
+              'Icp/PointToPlaneRadius', '0',
+              'Icp/MaxTranslation', '2',
+              'Icp/MaxCorrespondenceDistance', '1',
+              'Icp/Strategy', '1',
+              'Icp/OutlierRatio', '0.7',
+              'Icp/CorrespondenceRatio', '0.01',
+              'Odom/ScanKeyFrameThr', '0.4',
+              'Odom/ResetCountdown', '1',
+              'OdomF2M/ScanSubtractRadius', '0.1',
+              'OdomF2M/ScanMaxSize', '60000',
+              'OdomF2M/BundleAdjustment', 'false',
+            ])
     ])
